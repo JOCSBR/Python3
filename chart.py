15e9f59d617bf4754d9adcfeb1cdb511a0ff163d
JOCSBR <josecsgsouza@gmail.com>
2019-10-25 18:31:42 -0300

    first commit

--- chart.py
+++ chart.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-x = [1,2,3,4,5,6]
-y = [1500, 1900, 700, 1100, 2000, 1800]
-plt.plot(x,y)
-plt.show()
+# import matplotlib.pyplot as plt
+# x = [1,2,3,4,5,6]
+# y = [1500, 1900, 700, 1100, 2000, 1800]
+# plt.plot(x,y)
+# plt.show()
+print('OK')
